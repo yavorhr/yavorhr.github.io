f6cd7d48c5066046680f2f70f9b157909b70db7c
wolfie <yavorhristozov@gmail.com>
2022-04-12 15:58:09 +0300

    Navigation setup
    Add .JS to show and hide navigation menu ; add CSS to  nav links

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/* ========== SHOW MENU ========== */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId)
+    const nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            // to add the show-menu class to the div tag with the nav_menu class
+            nav.classList.toggle('show-menu');
+        })
+    };
+}
+showMenu('nav-toggle', 'nav-menu');
+
+/* ========== REMOVE MENU MOBILE ========== */
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    //when we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/* ========== SCROLL SECTIONS ACTIVE LINK ========== */
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
