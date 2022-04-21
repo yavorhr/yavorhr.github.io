e916a06ccd9e7d9c7ea06d8d0e21beed28f15579
wolfie <yavorhristozov@gmail.com>
2022-04-21 18:54:35 +0300

    Generate HTML to PDF
    Add html2pdf.bundle.min.js script
    Add Generate PDF functionality in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,3 +85,43 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme());
     localStorage.setItem('selected-icon', getCurrentIcon());
 })
+
+/* ========== REDUCE THE SIZE AND PRINT ON A4 SHEET ========== */
+function scaleCv() {
+    document.body.classList.add('scale-cv')
+}
+
+/* ========== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ========== */
+function removeScale() {
+    document.body.classList.remove('scale-cv')
+}
+
+/* ========== GENERATE PDF ========== */
+//PDF generated area
+let areaCv = document.getElementById('area-cv')
+let resumeButton = document.getElementById('resume-button');
+
+
+//Html2pdf options
+let opt = {
+    margin: 0,
+    filename: 'YavorHristozov_Resume.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 4 },
+    jsPDF: { format: 'a4', orientation: 'portrait' }
+};
+
+//Function to call areaCv and html2Pdf options
+function generateResume() {
+    html2pdf(areaCv, opt)
+}
+
+// When the button is clicked, it executes the three functions
+resumeButton.addEventListener('click', () => {
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the
+    scaleCv();
+    // 2. The PDF is generated
+    generateResume();
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal
+    setTimeout(removeScale, 5000);
+})
